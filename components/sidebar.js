8db6756853bc3bf5af097f8eacd60000f1dc4098
rizkymakruf <armppy8@gmail.com>
2022-11-15 19:42:41 +0700

    sidebar

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,9 +1,21 @@
+import React from "react";
+import Image from "next/image";
+import ProfilePic from "../public/pp1.png";
+
 const Sidebar = () => {
+  const [menu, setMenu] = React.useState("");
+
+  const onClick = React.useCallback(
+    (m) => () => {
+      setMenu(m);
+    },
+    [menu]
+  );
   return (
     <div className="w-3/12 bg-gray-500 h-screen fixed left-0">
-      <div className="w-full flex flex-col justify-center px-10">
+      <div className="w-full flex flex-col justify-center">
         {/* name app  */}
-        <div className="flex gap-2 items-center justify-center py-8">
+        <div className="flex gap-2 items-center justify-center py-8 px-10">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -19,8 +31,9 @@ const Sidebar = () => {
           <p className="text-lg font-bold text-white">MyDashboard</p>
         </div>
         {/* profile pic */}
-        <div className="flex flex-col justify-center items-center pt-5 pb-16 space-y-2">
+        <div className="flex flex-col justify-center items-center px-10 pt-5 pb-16 space-y-2">
           <div className="w-32 h-32 rounded-full bg-white relative">
+            <Image src={ProfilePic} className={"rounded-full"} />
             <div className="w-8 h-8 bg-black/90 rounded-full flex justify-center items-center absolute bottom-1 right-1">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -36,37 +49,84 @@ const Sidebar = () => {
           <p className="text-white text-lg">Jennifer Sanss</p>
         </div>
         {/* list menu */}
-        <div className="flex gap-2 items-center px-3 py-3 bg-green-200">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="w-5 h-5 text-white"
+        <div className="w-full px-5 md:px-10">
+          <div
+            className={`flex gap-2 items-center px-3 py-3 cursor-pointer justify-center md:justify-start ${
+              menu === "dashboard" ? "bg-white/40" : ""
+            } duration-200 rounded-xl`}
+            onClick={onClick("dashboard")}
           >
-            <path
-              fill-rule="evenodd"
-              d="M7.5 5.25a3 3 0 013-3h3a3 3 0 013 3v.205c.933.085 1.857.197 2.774.334 1.454.218 2.476 1.483 2.476 2.917v3.033c0 1.211-.734 2.352-1.936 2.752A24.726 24.726 0 0112 15.75c-2.73 0-5.357-.442-7.814-1.259-1.202-.4-1.936-1.541-1.936-2.752V8.706c0-1.434 1.022-2.7 2.476-2.917A48.814 48.814 0 017.5 5.455V5.25zm7.5 0v.09a49.488 49.488 0 00-6 0v-.09a1.5 1.5 0 011.5-1.5h3a1.5 1.5 0 011.5 1.5zm-3 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-              clip-rule="evenodd"
-            />
-            <path d="M3 18.4v-2.796a4.3 4.3 0 00.713.31A26.226 26.226 0 0012 17.25c2.892 0 5.68-.468 8.287-1.335.252-.084.49-.189.713-.311V18.4c0 1.452-1.047 2.728-2.523 2.923-2.12.282-4.282.427-6.477.427a49.19 49.19 0 01-6.477-.427C4.047 21.128 3 19.852 3 18.4z" />
-          </svg>
-          <p className="text-white">Dashboard</p>
-        </div>
-        <div className="flex gap-2 items-center px-3 py-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="w-5 h-5 text-white"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-5 h-5 text-white"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.5 5.25a3 3 0 013-3h3a3 3 0 013 3v.205c.933.085 1.857.197 2.774.334 1.454.218 2.476 1.483 2.476 2.917v3.033c0 1.211-.734 2.352-1.936 2.752A24.726 24.726 0 0112 15.75c-2.73 0-5.357-.442-7.814-1.259-1.202-.4-1.936-1.541-1.936-2.752V8.706c0-1.434 1.022-2.7 2.476-2.917A48.814 48.814 0 017.5 5.455V5.25zm7.5 0v.09a49.488 49.488 0 00-6 0v-.09a1.5 1.5 0 011.5-1.5h3a1.5 1.5 0 011.5 1.5zm-3 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                clip-rule="evenodd"
+              />
+              <path d="M3 18.4v-2.796a4.3 4.3 0 00.713.31A26.226 26.226 0 0012 17.25c2.892 0 5.68-.468 8.287-1.335.252-.084.49-.189.713-.311V18.4c0 1.452-1.047 2.728-2.523 2.923-2.12.282-4.282.427-6.477.427a49.19 49.19 0 01-6.477-.427C4.047 21.128 3 19.852 3 18.4z" />
+            </svg>
+            <p className="text-white hidden md:block">Dashboard</p>
+          </div>
+          <div
+            className={`flex gap-2 items-center px-3 py-3 cursor-pointer justify-center md:justify-start ${
+              menu === "folders" ? "bg-white/40" : ""
+            } duration-200 rounded-xl`}
+            onClick={onClick("folders")}
           >
-            <path
-              fill-rule="evenodd"
-              d="M7.5 5.25a3 3 0 013-3h3a3 3 0 013 3v.205c.933.085 1.857.197 2.774.334 1.454.218 2.476 1.483 2.476 2.917v3.033c0 1.211-.734 2.352-1.936 2.752A24.726 24.726 0 0112 15.75c-2.73 0-5.357-.442-7.814-1.259-1.202-.4-1.936-1.541-1.936-2.752V8.706c0-1.434 1.022-2.7 2.476-2.917A48.814 48.814 0 017.5 5.455V5.25zm7.5 0v.09a49.488 49.488 0 00-6 0v-.09a1.5 1.5 0 011.5-1.5h3a1.5 1.5 0 011.5 1.5zm-3 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-              clip-rule="evenodd"
-            />
-            <path d="M3 18.4v-2.796a4.3 4.3 0 00.713.31A26.226 26.226 0 0012 17.25c2.892 0 5.68-.468 8.287-1.335.252-.084.49-.189.713-.311V18.4c0 1.452-1.047 2.728-2.523 2.923-2.12.282-4.282.427-6.477.427a49.19 49.19 0 01-6.477-.427C4.047 21.128 3 19.852 3 18.4z" />
-          </svg>
-          <p className="text-white">Dashboard</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-5 h-5 text-white"
+            >
+              <path d="M19.5 21a3 3 0 003-3v-4.5a3 3 0 00-3-3h-15a3 3 0 00-3 3V18a3 3 0 003 3h15zM1.5 10.146V6a3 3 0 013-3h5.379a2.25 2.25 0 011.59.659l2.122 2.121c.14.141.331.22.53.22H19.5a3 3 0 013 3v1.146A4.483 4.483 0 0019.5 9h-15a4.483 4.483 0 00-3 1.146z" />
+            </svg>
+            <p className="text-white hidden md:block">Folders</p>
+          </div>
+          <div
+            className={`flex gap-2 items-center px-3 py-3 cursor-pointer justify-center md:justify-start ${
+              menu === "favorites" ? "bg-white/40" : ""
+            } duration-200 rounded-xl`}
+            onClick={onClick("favorites")}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-5 h-5 text-white"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.25 2.25a3 3 0 00-3 3v4.318a3 3 0 00.879 2.121l9.58 9.581c.92.92 2.39 1.186 3.548.428a18.849 18.849 0 005.441-5.44c.758-1.16.492-2.629-.428-3.548l-9.58-9.581a3 3 0 00-2.122-.879H5.25zM6.375 7.5a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <p className="text-white hidden md:block">Favorites</p>
+          </div>
+          <div
+            className={`flex gap-2 items-center px-3 py-3 cursor-pointer justify-center md:justify-start ${
+              menu === "settings" ? "bg-white/40" : ""
+            } duration-200 rounded-xl`}
+            onClick={onClick("settings")}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-5 h-5 text-white"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 00-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 00-2.282.819l-.922 1.597a1.875 1.875 0 00.432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 000 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 00-.432 2.385l.922 1.597a1.875 1.875 0 002.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 002.28-.819l.923-1.597a1.875 1.875 0 00-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 000-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 00-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 00-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 00-1.85-1.567h-1.843zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <p className="text-white hidden md:block">Settings</p>
+          </div>
         </div>
       </div>
     </div>

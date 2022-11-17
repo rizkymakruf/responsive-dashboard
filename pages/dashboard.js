396a173fe8a7ac818ca5a48b8e89aeca599301fb
rizkymakruf <armppy8@gmail.com>
2022-11-17 21:46:04 +0700

    overflow

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -10,10 +10,28 @@ const Dashboard = () => {
             Create folders to short file and have quick access to documents
           </p>
         </div>
-        <div className="w-7/12 flex relative bg-gray-500">
-          <div className="overflow-x-scroll flex">
-            <div className="flex-none w-32 bg-black">
-              <div className="flex flex-col space-y-2">
+        <div className="w-7/12 flex relative bg-gray-500 p-2">
+          <div className="overflow-x-scroll overflow-auto flex gap-3">
+            <div className="flex flex-none justify-center items-center w-32 bg-black">
+              <div className="flex flex-col space-y-2 justify-center items-center">
+                <div className="w-12 h-12 rounded-full bg-yellow-600"></div>
+                <p className="text-white">name</p>
+              </div>
+            </div>
+            <div className="flex flex-none justify-center items-center w-32 bg-black">
+              <div className="flex flex-col space-y-2 justify-center items-center">
+                <div className="w-12 h-12 rounded-full bg-yellow-600"></div>
+                <p className="text-white">name</p>
+              </div>
+            </div>
+            <div className="flex flex-none justify-center items-center w-32 bg-black">
+              <div className="flex flex-col space-y-2 justify-center items-center">
+                <div className="w-12 h-12 rounded-full bg-yellow-600"></div>
+                <p className="text-white">name</p>
+              </div>
+            </div>
+            <div className="flex flex-none justify-center items-center w-32 bg-black">
+              <div className="flex flex-col space-y-2 justify-center items-center">
                 <div className="w-12 h-12 rounded-full bg-yellow-600"></div>
                 <p className="text-white">name</p>
               </div>

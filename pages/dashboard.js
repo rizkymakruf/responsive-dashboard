d540cfed0dbde9c1ae5b7e523ea4d7e805046237
rizkymakruf <armppy8@gmail.com>
2022-11-21 22:03:17 +0700

    intial commit

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -19,6 +19,22 @@ const Dashboard = () => {
                   <p className="text-white">:</p>
                 </div>
               </div>
+              <div className="absolute w-full bottom-0">
+                <div className="w-full flex flex-col py-3 px-5">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-12 h-12 text-white"
+                  >
+                    <path d="M11.644 1.59a.75.75 0 01.712 0l9.75 5.25a.75.75 0 010 1.32l-9.75 5.25a.75.75 0 01-.712 0l-9.75-5.25a.75.75 0 010-1.32l9.75-5.25z" />
+                    <path d="M3.265 10.602l7.668 4.129a2.25 2.25 0 002.134 0l7.668-4.13 1.37.739a.75.75 0 010 1.32l-9.75 5.25a.75.75 0 01-.71 0l-9.75-5.25a.75.75 0 010-1.32l1.37-.738z" />
+                    <path d="M10.933 19.231l-7.668-4.13-1.37.739a.75.75 0 000 1.32l9.75 5.25c.221.12.489.12.71 0l9.75-5.25a.75.75 0 000-1.32l-1.37-.738-7.668 4.13a2.25 2.25 0 01-2.134-.001z" />
+                  </svg>
+                  <p className="text-white">Marketing</p>
+                  <p className="text-white/50 text-sm">126Mb</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
